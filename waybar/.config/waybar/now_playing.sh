dbaad613590c91eb23aa1ce7734d380a1db39169
zgfif <zihzag@gmail.com>
2025-07-16 13:31:59 +0300

    change font to SF Pro and SF Mono

--- waybar/.config/waybar/now_playing.sh
+++ waybar/.config/waybar/now_playing.sh
@@ -1,3 +1,17 @@
 #! /usr/bin/bash
 
-echo $(pactl list sink-inputs | grep 'media.name' | cut -d '=' -f2 | sed 's/^ "//;s/"$//' | tail -n 1);
+pactl list sink-inputs \
+	| grep 'media.name' \
+	| cut -d '=' -f2 \
+	| sed -e 's/^ "//' -e 's/"$//' -e 's/&/\&amp;/g' \
+	| tail -n 1
+
+# retrieves the list of sink-inputs
+# extracts only with 'media.name'
+# removes " in the start and " in the end and replaces all & with \&amp; for correct gtk view
+# takes only 1 last media.name
+
+
+
+
+
